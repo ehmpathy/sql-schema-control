b9aad29f4060448c5b86365fd4c824f1778d5b33
UladKasach <nappa300@gmail.com>
2020-03-25 06:28:23 -0500

    fix(apply): remove the aesthetic delay on apply

--- src/logic/plan/applyPlan/applyPlan.ts
+++ src/logic/plan/applyPlan/applyPlan.ts
@@ -2,9 +2,7 @@ import { DatabaseConnection, DefinitionPlan, ChangeDefinition, ResourceDefinitio
 import { applyPlanForChange } from './applyPlanForChange';
 import { applyPlanForResource } from './applyPlanForResource';
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export const applyPlan = async ({ connection, plan }: { connection: DatabaseConnection; plan: DefinitionPlan }) => {
-  await sleep(150); // purely aesthetic; TODO: make optional
   if (plan.definition.constructor === ChangeDefinition) return applyPlanForChange({ connection, plan });
   if (plan.definition.constructor === ResourceDefinition) return applyPlanForResource({ connection, plan });
   throw new Error('unexpected definition type');
