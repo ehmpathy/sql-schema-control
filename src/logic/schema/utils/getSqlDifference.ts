90cadb022a2c5e73766d957a8b88f76564a73a77
UladKasach <nappa300@gmail.com>
2020-04-10 05:40:06 -0500

    fix(diff): replace diff w/ jest-diff for better visual

--- src/logic/schema/utils/getSqlDifference.ts
+++ src/logic/schema/utils/getSqlDifference.ts
@@ -1,23 +1,11 @@
-import chalk from 'chalk';
-import * as diff from 'diff';
+import { diffStringsUnified } from 'jest-diff';
 
 export const getSqlDifference = ({ oldSql, newSql }: { oldSql: string; newSql: string }) => {
   // check strings are not equivalent
   if (oldSql === newSql) return null; // i.e., no difference
 
   // compute the differences
-  const sqlDiffParts = diff.diffLines(oldSql, newSql);
-
-  // merge the differences into a string, with color
-  const sqlDiffString = sqlDiffParts.reduce((summary, thisPart) => {
-    // pick the color
-    let chalkMethod = chalk.gray;
-    if (thisPart.added) chalkMethod = chalk.green;
-    if (thisPart.removed) chalkMethod = chalk.red;
-
-    // append the colored string
-    return summary + chalkMethod(thisPart.value);
-  }, ''); // tslint:disable-line align
+  const sqlDiffString = diffStringsUnified(oldSql.trim(), newSql.trim());
 
   // return the diff string
   return sqlDiffString;
