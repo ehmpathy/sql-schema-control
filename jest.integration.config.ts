b65149206af1b5517a212c44eb43d7cf92a63d24
Ulad Kasach <uladkasach@gmail.com>
2024-08-03 12:53:00 -0400

    fix(practs): bump practs to latest best

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'jest';
 // ensure tests run in utc, like they will on cicd and on server; https://stackoverflow.com/a/56277249/15593329
 process.env.TZ = 'UTC';
 
+// ensure tests run like on local machines, so snapshots are equal on local && cicd
+process.env.FORCE_COLOR = 'true';
+
 // https://jestjs.io/docs/configuration
 const config: Config = {
   verbose: true,
--- jest.unit.env.ts
+++ jest.unit.env.ts
@@ -1,8 +1,12 @@
-import { stage, Stage } from './src/utils/environment';
-
 /**
- * sanity check that unit tests are only run in 'test' environment
- * - if they are run in prod environment, we could load a bunch of junk data into our prod databases, which would be no bueno
+ * sanity check that unit tests are only run the 'test' environment
+ *
+ * usecases
+ * - prevent polluting prod state with test data
+ * - prevent executing financially impacting mutations
  */
-if (stage !== Stage.TEST && process.env.I_KNOW_WHAT_IM_DOING !== 'true')
-  throw new Error(`unit-test is not targeting stage 'test'`);
+if (
+  (process.env.NODE_ENV !== 'test' || process.env.STAGE) &&
+  process.env.I_KNOW_WHAT_IM_DOING !== 'true'
+)
+  throw new Error(`unit.test is not targeting stage 'test'`);
